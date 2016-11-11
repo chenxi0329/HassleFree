1aa627ff6907ef64d0834401bf755dc03761e760
Chen <chenxi0329@gmail.com>
2016-11-10 22:51:34 -0500

    New LCs
    58/136

---
+++ length-of-last-word.py
@@ -0,0 +1,17 @@
+class Solution:
+    #58
+    def lengthOfLastWord0(self, str):
+        return len(str.strip().split(" ")[-1])
+
+    def lengthOfLastWord1(self, str):
+        length = 0
+        str=str.strip()
+        for char in reversed(str):
+            if char != " ":
+                length += 1
+            else:
+                break
+        return length
+if __name__ == "__main__":
+    print Solution().lengthOfLastWord0("Ola Mi Amigo  ")
+    print Solution().lengthOfLastWord1("  nong sha lei  ")
---
+++ single-number.py
@@ -0,0 +1,21 @@
+import operator
+class Solution:
+    def singleNumber0(self,nums):
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+
+        for key,value in count.items():
+            if value == 1:
+                return key
+
+    def singleNumber1(self,nums):
+        return reduce(lambda key,value:key^value, nums)
+
+    def singleNumber2(self, nums):
+        return reduce(operator.xor, nums)
+
+if __name__ == '__main__':
+    print Solution().singleNumber0([1, 1, 2, 2, 3])
+    print Solution().singleNumber1([1, 1, 2, 2, 3])
+    print Solution().singleNumber2([1, 1, 2, 2, 3])
